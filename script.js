f33cc99e628fbae35f2bb0575d7b829b8e7e7af2
Eimrine's Point of View <eimrine@users.noreply.github.com>
2014-11-01 23:48:14 +0200

    Update script.js
    number feature done

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 var numberList = [1, -9, 0, 24, 55, -3];
 
-var shuffle = document.getElementById("shubbleButton");
-var numbers = document.getElementById("numbersButton")
+var shuffle = document.getElementById("shuffleButton");
+
+var numberListNegative = []; 
+var numbers = document.getElementById("numbersButton");
+numbers.onclick=function(){
+//obj.addEventListener("click", 
+    alert("hr!");
+    console.log(numberList); 
+    for (i=0;i<numberList.length;i++){
+        if (numberList[i]<0){
+            numberListNegative.push(numberList[i]);
+        }
+    }
+};
+//);
+
+
+console.log(numberListNegative); 
